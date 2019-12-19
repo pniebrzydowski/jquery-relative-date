91e37ad10dff65377925fd34d1bae60d58a38c0d
Patrick Niebrzydowski <patrick.niebrzydowski@tourradar.com>
2019-12-19 15:54:38 +0100

    add new fields and value handlers
    * add number and select field on create
    * parse existing values based on now-nt format
    * update the original field value on change of subfields
    * add unit options to init config options

--- relative-date.js
+++ relative-date.js
@@ -1,22 +1,79 @@
 (function($, window, document, undefined) {
   var pluginName = "relativeDate";
 
+  parseValue = function(textValue) {
+    if (!textValue) {
+      return { unit: "", number: "" };
+    }
+    var values = textValue.split("-")[1];
+    var unit = values.slice(values.length - 1);
+    var number = values.slice(0, values.length - 1);
+    return {
+      unit: unit,
+      number: number
+    };
+  };
+
+  getValueFromParts = function(number, unit) {
+    if (!number | !unit) {
+      return "now";
+    }
+    return "now-" + number + unit;
+  };
+
   RelativeDate = function(element, options, dataName) {
+    var self = this;
     this.element = element;
     this.$element = $(element);
     this.options = options;
     this.dataName = dataName;
+    this.unitOptions = options.unitOptions;
 
-    (this._events = function() {
-      return this;
-    }),
-      (this.create = function() {
-        var value = this.$element.val();
-        if (!value) {
-          this.$element.val(this.options.defaultValue);
-        }
-        return this;
+    this._updateValue = function() {
+      var number = self.$numberField.val();
+      var unit = self.$unitField.val();
+      var newValue = getValueFromParts(number, unit);
+      self.$element.val(newValue).change();
+    };
+
+    this._addEventListeners = function() {
+      this.$numberField.on("change", self._updateValue);
+      this.$unitField.on("change", self._updateValue);
+    };
+
+    this.create = function() {
+      var value = self.$element.val();
+      if (!value) {
+        self.$element.val(self.options.defaultValue);
+      }
+
+      var parsedValue = parseValue(value);
+
+      self.$element.css("display", "none");
+      self.numberField = document.createElement("input");
+      self.numberField.setAttribute("type", "number");
+      self.numberField.setAttribute("min", 0);
+      self.$numberField = $(self.numberField);
+      self.$numberField.val(parsedValue.number);
+
+      var opts = [];
+      self.unitOptions.forEach(opt => {
+        var newField = document.createElement("option");
+        newField.setAttribute("value", opt.value);
+        $(newField).html(opt.label);
+        opts.push($(newField));
       });
+
+      self.unitField = document.createElement("select");
+      self.$unitField = $(self.unitField).html(opts);
+      self.$unitField.val(parsedValue.unit);
+
+      self.$element.after(self.$numberField, self.$unitField);
+
+      self._addEventListeners();
+      return self;
+    };
+
     return this;
   };
 
@@ -46,6 +103,20 @@
   };
 
   $.fn[pluginName].options = {
-    defaultValue: ""
+    defaultValue: "",
+    unitOptions: [
+      {
+        value: "d",
+        label: "day(s) ago"
+      },
+      {
+        value: "w",
+        label: "week(s) ago"
+      },
+      {
+        value: "m",
+        label: "month(s) ago"
+      }
+    ]
   };
 })(jQuery, window, document);
